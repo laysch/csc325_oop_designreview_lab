b3d23f82aab6d0df98e7727d3bdc2aabd0e882fe
laila <100658865+laysch@users.noreply.github.com>
2024-07-03 05:28:12 -0400

    Update Student.java

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,12 +8,44 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
+public class Student extends Human { // ToDo 1: Make this class a child of Human
 
-	// ToDo 2: Fix the resulting errors
+    // ToDo 3: Add a field for GPA and create a setter and a getter
+    private double gpa;
 
-	// ToDo 3: Add a field for GPA and create a setter and a getter
-	
-	// ToDo 4: Add comments to your code
+    // ToDo 6: Fix the constructor of the Student class
+    public Student(String name, short age, double gpa) {
+        super(name, age);
+        this.gpa = gpa;
+    }
+
+    // ToDo 9: Add a toString method for the Student class
+    @Override
+    public String toString() {
+        return "Student{name=" + getName() + ", age=" + getAge() + ", gpa=" + gpa + "}";
+    }
+
+    // Inherited methods from Human class (getAddress, setAddress) remain unchanged.
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    /**
+     * Setter method to modify the student's GPA.
+     *
+     * @param gpa The new GPA value
+     */
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
